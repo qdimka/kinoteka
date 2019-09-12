3f929e21fb8e43da8092078a21923c220b584b7e
Dmitry Dmitriev <qdimka1991@mail.ru>
2019-09-12 10:08:59 +0300

    Added reactstrap

--- src/App.js
+++ src/App.js
@@ -4,14 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Head</h1>
-        <h2>Head</h2>
-        <h3>Head</h3>
-        <h4>Head</h4>
-        <h5>Head</h5>
-        <h6>Head</h6>
-      </header>
+      
     </div>
   );
 }
---
+++ src/components/SideBar/SideBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const sideBar = () => (
+    <div className="sidebar">
+
+    </div>
+);
+
+export default sideBar;
