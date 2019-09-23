71e920657091153c27c4d68c0dd3c0043d26acae
Dmitry Dmitriev <dd@smarthead.ru>
2019-09-23 15:02:57 +0300

    ! moved to electron app

---
+++ app/menu.js
@@ -0,0 +1,10 @@
+// @flow
+import { app, Menu, shell, BrowserWindow } from 'electron';
+
+const buildMenu = () => buildDefaultTemplate();
+
+const buildDefaultTemplate = () => {
+  return [];
+}
+
+export default buildMenu;
