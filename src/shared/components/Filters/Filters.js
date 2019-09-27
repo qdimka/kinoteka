05e93e05ad6652e4fb2f1b0c716c1464cc2a800b
Dmitry Dmitriev <qdimka1991@mail.ru>
2019-09-27 15:05:39 +0300

    Реализация компонента с фильтрами

--- src/shared/components/Filters/Filters.js
+++ src/shared/components/Filters/Filters.js
@@ -1,22 +1,13 @@
 import React, { useState } from 'react';
 import filters from './data';
 import { Button, Navbar } from 'reactstrap';
-import DropdownItem from 'reactstrap/lib/DropdownItem';
-import DropdownMenu from 'reactstrap/lib/DropdownMenu';
-import DropdownToggle from 'reactstrap/lib/DropdownToggle';
-import UncontrolledDropdown from 'reactstrap/lib/UncontrolledDropdown';
-import NavItem from 'reactstrap/es/NavItem';
 import Nav from 'reactstrap/lib/Nav';
 import Collapse from 'reactstrap/lib/Collapse';
-import NavbarToggler from 'reactstrap/lib/NavbarToggler';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAlignLeft, faFilter } from '@fortawesome/free-solid-svg-icons';
-import NavLink from 'reactstrap/es/NavLink';
+import { faFilter } from '@fortawesome/free-solid-svg-icons';
 import NavbarBrand from 'reactstrap/es/NavbarBrand';
 import RangeFilter from './RangeFilter';
 import Form from 'reactstrap/es/Form';
-import FormGroup from 'reactstrap/es/FormGroup';
-import Label from 'reactstrap/es/Label';
 
 const Filters = ({ data }) => {
   data = filters;
@@ -32,7 +23,7 @@ const Filters = ({ data }) => {
       <Collapse isOpen={isOpen} navbar>
         <Nav className="ml-auto" navbar>
           <Form>
-            <RangeFilter/>
+            <RangeFilter title={"Title"}/>
           </Form>
         </Nav>
       </Collapse>
--- src/shared/components/Filters/RangeFilter.js
+++ src/shared/components/Filters/RangeFilter.js
@@ -2,27 +2,35 @@ import React from 'react';
 import NavItem from 'reactstrap/es/NavItem';
 import Label from 'reactstrap/es/Label';
 import Input from 'reactstrap/es/Input';
+import FormGroup from 'reactstrap/es/FormGroup';
+import FormText from 'reactstrap/es/FormText';
 
-const RangeFilter = ({title}) => (
-  <NavItem>
-    <div className='filter-header'>{title}</div>
-    <Label>From</Label>
-    <Input type="select" name="select" id="exampleSelect">
-      <option>1</option>
-      <option>2</option>
-      <option>3</option>
-      <option>4</option>
-      <option>5</option>
-    </Input>
-    <Label>To</Label>
-    <Input type="select" name="select" id="exampleSelect">
-      <option>1</option>
-      <option>2</option>
-      <option>3</option>
-      <option>4</option>
-      <option>5</option>
-    </Input>
-  </NavItem>
+const RangeFilter = ({ title }) => (
+  <>
+    <FormText>
+      {title}
+    </FormText>
+    <FormGroup>
+      <Label for="to">From</Label>
+      <Input type="select" name="select" id="to">
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+        <option>4</option>
+        <option>5</option>
+      </Input>
+    </FormGroup>
+    <FormGroup>
+      <Label for="to">To</Label>
+      <Input type="select" name="select" id="to">
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+        <option>4</option>
+        <option>5</option>
+      </Input>
+    </FormGroup>
+  </>
 );
 
 export default RangeFilter;
